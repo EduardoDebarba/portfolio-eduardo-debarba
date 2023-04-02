c63d51e373f50903ab161c40dc951403f7f961b4
Eduardo Debarba Scheuermann <eduardodsdev@gmail.com>
2023-04-02 14:21:28 -0300

    implemented scroll animation on the page and added new projects

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ menuToggle.addEventListener("click", () => {
     menuSection.classList.toggle("on", show)
     show = !show;
 })
+
+let cont = document.querySelector('.more-projects')
+
+let btnClick = function(){
+  if(cont.style.display === 'flex'){
+    cont.style.display = 'none'
+  }else{
+    cont.style.display = 'flex'
+  }
+}
+
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('.scroll', {duration: 2000})
